2da59a6ec045a67190427fa6ab69c5c500eb4989
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-04-18 14:51:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "ibm_container_cluster" "cluster" {
   name              = "${var.cluster_name}${random_id.name.hex}"
-  datacenter        = "${var.datacenter}"
+  datacenter        = "dal13"
   default_pool_size = "${var.default_pool_size}"
   machine_type      = "${var.machine_type}"
   hardware          = "${var.hardware}"
   kube_version      = "${var.kube_version}"
-  public_vlan_id    = "${var.public_vlan_id}"
-  private_vlan_id   = "${var.private_vlan_id}"
-  resource_group_id = "5b0134601c214ccd90ab9221227c56e6"
+  public_vlan_id    = "2851976"
+  private_vlan_id   = "2851978"
+  resource_group_id = "6764d0853c60419180ba88daafce32c4"
 }
 
 resource "random_id" "name" {
